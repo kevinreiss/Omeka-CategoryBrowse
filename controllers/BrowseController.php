3a9daeb1f6cfd409a777ceffef65e58d42a52f7a
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-03-21 00:00:00 +0100

    Upgraded for Omeka 2.0.

--- controllers/BrowseController.php
+++ controllers/BrowseController.php
@@ -1,24 +1,35 @@
 <?php
-
-/* 
- * CategoryBrowse_ElbrowseController.php
- * 
+/**
  * Enables browsing of element values by element name and element text
+ *
+ * @package CategoryBrowse
  */
-
-class CategoryBrowse_BrowseController extends Omeka_Controller_Action
+class CategoryBrowse_BrowseController extends Omeka_Controller_AbstractActionController
 {
+    protected $_autoCsrfProtection = true;
+
+    protected $_browseRecordsPerPage = 100;
+
+    public function init()
+    {
+        $this->_helper->db->setDefaultModelName('ElementText');
+    }
+
 	public function browseAction()
     {
-     	$elementTextTable = $this->getDb()->getTable('ElementText');
-       	$itemTable = $this->getDb()->getTable('Item');
-        $elementText = urldecode($this->_getParam('eltext'));
+        $db = $this->_helper->db;
+        $itemTable = $db->getTable('Item');
+        $elementTextTable = $db->getTable('ElementText');
+        $elementTable = $db->getTable('Element');
+
         $elementSetName = $this->_unslugify($this->_getParam('elset'));
         $elementName = $this->_unslugify($this->_getParam('elname'));
-        $el = $this->getDb()->getTable('Element')->findByElementSetNameAndElementName($elementSetName, $elementName);
-        $select = $elementTextTable->getSelect()->where('text = ? ', $elementText)->where('element_id = ?', $el->id);
-          
-        release_object($el);
+        $el = $elementTable->findByElementSetNameAndElementName($elementSetName, $elementName);
+
+        $elementText = urldecode($this->_getParam('eltext'));
+        $select = $elementTextTable->getSelect()
+            ->where('text = ? ', $elementText)
+            ->where('element_id = ?', $el->id);
         $elTexts = $elementTextTable->fetchObjects($select);
 
         $records = array();
@@ -26,54 +37,69 @@ class CategoryBrowse_BrowseController extends Omeka_Controller_Action
        		$records[] = $itemTable->find($elText->record_id);
         }
 
-        $this->view->assign(array('items'=>$records, 'eltext'=>$elementText));
-
+        $this->view->assign(array(
+            'items' => $records,
+            'eltext' => $elementText,
+            'element' => $el,
+        ));
     }
 
     public function listAction()
     {
-         
-    	 $elementSetName = $this->_getParam('elset');
-         $elementName = $this->_getParam('elname');
-         $curPage = $this->_getParam('page');
-         $elementTextTable = $this->getDb()->getTable('ElementText');
-         $el = $this->getDb()->getTable('Element')->findByElementSetNameAndElementName($this->_unslugify($elementSetName), $this->_unslugify($elementName));
-         //$elTexts = $this->getDb()->getTable('ElementText')->findByElement($el->id);
-         $select = $elementTextTable->getSelect()->where('element_id = ?', $el->id)->order('text')->group('text');
-         
-         $elTexts = $elementTextTable->fetchObjects($select);
-         //$sortedTexts = $elTexts->getSelect()->findByElement($el->id)->order('text');
-       	 $totalCategories = count($elTexts);
-         release_object($el);
-         
-         // set-up pagination routine
-         $paginationUrl = $this->getRequest()->getBaseUrl().'/categories/list/' . $elementSetName . '/' . $elementName . "/";
-         /*
-         $pageAdapter = new Zend_Paginator_Adapter_DbSelect($elementTextTable->getSelect()->where('element_id = ?', $el->id)->order('text')->group('text'));
-         */
-         $paginator = Zend_Paginator::factory($select);
-         $paginator->setItemCountPerPage(20);
-         //$totalCategories = count($paginator);
-         $paginator->setCurrentPageNumber($curPage);
-         $this->view->paginator = $paginator;
-           //Serve up the pagination
-         // add other pagination items
-         $pagination = array(
-                            'page'          => $curPage, 
-                            'per_page'      => 20, 
-                            'total_results' => $totalCategories, 
-                            'link'          => $paginationUrl);
-        
-         
-         Zend_Registry::set('pagination', $pagination);
-         //$this->view->assign(array('texts'=>$elTexts, 'elset'=>$elementSetName, 'elname'=>$elementName, 'total_results'=>$totalCategories));
-		$this->view->assign(array('elset'=>$elementSetName, 'elname'=>$elementName, 'total_results'=>$totalCategories));
+        $db = $this->_helper->db;
+        $elementTextTable = $db->getTable('ElementText');
+        $elementTable = $db->getTable('Element');
+
+        $elementSetName = $this->_unslugify($this->_getParam('elset'));
+        $elementName = $this->_unslugify($this->_getParam('elname'));
+        $el = $elementTable->findByElementSetNameAndElementName($elementSetName, $elementName);
+
+        // Get the records filtered to Omeka_Db_Table::applySearchFilters().
+        $params = array(
+            'element_id' => $el->id,
+            'sort_field' => 'text',
+            'sort_dir' => 'a',
+        );
+        $recordsPerPage = $this->_getBrowseRecordsPerPage();
+        $currentPage = $this->getParam('page', 1);
+
+        $select = $db->getSelectForFindBy($params);
+        $select->group('text');
+        $totalRecords = get_db()->query($select)->rowCount();
+        if ($currentPage) {
+            $select->limitPage($currentPage, $recordsPerPage);
+        } else {
+            $select->limit($recordsPerPage);
+        }
+        $elTexts = $elementTextTable->fetchObjects($select);
+
+        //$sortedTexts = $elTexts->getSelect()->findByElement($el->id)->order('text');
+        release_object($el);
+
+        // Add pagination data to the registry. Used by pagination_links().
+        if ($recordsPerPage) {
+            Zend_Registry::set('pagination', array(
+                'page' => $currentPage,
+                'per_page' => $recordsPerPage,
+                'total_results' => $totalRecords,
+            ));
+        }
+
+        $this->view->assign(array(
+            'elTexts' => $elTexts,
+            'elset' => $elementSetName,
+            'elname' => $elementName,
+            'total_results' => $totalRecords,
+        ));
      }
+
+    protected function _getBrowseDefaultSort()
+    {
+        return array('added', 'd');
+    }
 
      private function _unslugify($param)
      {
-         return ucwords(str_replace("-", " ", $param) );
+         return ucwords(str_replace('-', ' ', $param));
      }
 }
-
-?>
--- views/public/browse/browse.php
+++ views/public/browse/browse.php
@@ -1,75 +1,75 @@
 <?php
-/*
- * browseby.php 
- */
+$title = __('Browse by "%s"', $eltext);
+echo head(array(
+    'title' => $title,
+    'bodyclass' => 'category-browse browse',
+));
 ?>
-<?php $head = array('title' => 'Browse digitalMETRO by ' . $eltext, 'bodyclass' => 'categorybrowse'); ?>
-<?php head($head); ?>
-
 <div id="primary">
-<h1><?php echo $head['title']; ?></h1>
-
-<table class="results">
+    <h1><?php echo $title; ?></h1>
+    <table class="results">
 	    <tr>
-	  	<th>Sample Image</th>
-	    <th>Title</th>
-	    <th>Creator(s)</th>
-	    <th>Tags</th>  
+            <th><?php echo __('Sample Image'); ?></th>
+            <th><?php echo __('Title'); ?></th>
+            <th><?php echo __('Creator(s)'); ?></th>
+            <th><?php echo __('Tags'); ?></th>
+            <?php /* <th><?php echo __('Subjects'); ?></th> */ ?>
 	    </tr>
-		<?php while (loop_items()): ?>
-	
-			<tr>
-			<div class="item-row">
-				<div>
-					<?php if ($text = item('Item Type Metadata', 'Text')): ?>
-    				<?php $shortdesc = $text; ?>
-					<?php elseif ($description = item('Dublin Core', 'Description')): ?>
-    				<?php $shortdesc = $description; ?>
-				<?php endif; ?>	
-				<td>
-				<?php if (item_has_thumbnail()): ?>
-    				<div class="item-img">
-    				<?php echo link_to_item(item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'), 'width'=>'70', 'height'=>'70'))); ?>						
-    				</div>
-				<?php endif; ?>
-				</td>
-				
-				<td><div style="font-size: 1.1em;"><?php echo link_to_item(item('Dublin Core', 'Title'), array('class'=>'permalink', 'title'=>$shortdesc)); ?></div></td>
-				
-				
-				<td>
-				<?php if ($creator = item('Dublin Core', 'Creator', array('all'=>'true'))): ?>
-                    <?php foreach($creator as $inst) {?>
-                    	<div style="font-size: 1.1em;"><?php echo metadata_browser_create_link(metadata_browser_get_element_id("Creator"), $inst);?></div>
-                    <?php }?>
-                <?php endif; ?>
-				</td>
-				<td>
-                
-				<?php if (item_has_tags()): ?>
-    				<div><p style="font-size: .9em;">
-    				<?php echo item_tags_as_string(); ?></p>
-    				</div>
-				<?php endif; ?>
-				</td>
-				<!--
-				<td>
-				<?php //if ($subject = item('Dublin Core', 'Subject', array('all'=>'true'))): ?>
-                    <?php //foreach($subject as $sub) {?>
-                    <?php //echo create_browse_link("58", $sub);?>, 
-                    <?php //}?>
-                <?php //endif; ?>
-				</td>
-				-->
-				<?php echo plugin_append_to_items_browse_each(); ?>
-			
-				</div><!-- end class="item-meta" -->
-				
-			</div><!-- end class="item hentry" -->	
+		<?php
+        foreach (loop('items') as $item):
+            $title = metadata($item, array('Dublin Core', 'Title'));
+	    ?>
+		<tr class="item-row">
+			<td>
+			<?php if ($item->has_thumbnail): ?>
+				<div class="item-img">
+                    <?php echo link_to_item(
+                        item_image('square_thumbnail', array('alt' => $title, 'width' => '70', 'height' => '70'), 0, $item),
+                        array('class' => 'item-thumbnail'),
+                        'show',
+                        $item); ?>
+				</div>
+			<?php endif; ?>
+			</td>
+			<td>
+		        <div style="font-size: 1.1em;">
+                    <?php echo link_to_item(null, array('class' => 'permalink', 'title' => $title), 'show', $item); ?>
+	            </div>
+	        </td>
+			<td>
+			<?php
+            $creators = metadata($item, array('Dublin Core', 'Creator'), array('all' => 'true'));
+            $element = $item->getElement('Dublin Core', 'Creator');
+            foreach($creators as $creator): ?>
+                <div style="font-size: 1.1em;">
+                    <a class="browse-link" href="<?php
+                        $value = trim(strip_formatting($creator));
+                        $queryURL = url("items/browse?advanced[0][element_id]=" . $element->id . "&advanced[0][type]=contains&advanced[0][terms]=" . $value);
+                    ?>" title='Browse "<?php echo $value; ?>"'><?php echo $value; ?></a>
+                </div>
+            <?php endforeach; ?>
+			</td>
+			<td>
+			<?php
+            $tags = tag_string($item);
+			if ($tags): ?>
+				<div><p style="font-size: .9em;">
+			        <?php echo $tags; ?>
+		        </p></div>
+			<?php endif; ?>
+			</td>
+			<?php /*
+			<td>
+			<?php
+            $subjects = metadata($item, array('Dublin Core', 'Subject'), array('all' => 'true'));
+            foreach($subject as $sub) {
+                echo create_browse_link("58", $sub) . ',';
+            } ?>
+			</td>
+			*/ ?>
+            <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 		</tr>
-		<?php endwhile; ?>
-		</table>
-
+		<?php endforeach; ?>
+	</table>
 </div>
-
-<?php foot(); ?>
+<?php echo foot();
